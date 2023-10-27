dd6858229d1a61e4689bcb0a9abc44dccace6a0e
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-27 05:48:14 +0200

    feat: UPDATE schema and add mongoose to the module

--- back/src/modules/book/dto/create-book.dto.ts
+++ back/src/modules/book/dto/create-book.dto.ts
@@ -1,1 +1,30 @@
-export class CreateBookDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsMongoId, IsNotEmpty, IsPositive, IsString } from 'class-validator';
+import mongoose from 'mongoose';
+
+export class CreateBookDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  author: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsPositive()
+  price: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsMongoId()
+  sellerId: mongoose.Schema.Types.ObjectId;
+}
